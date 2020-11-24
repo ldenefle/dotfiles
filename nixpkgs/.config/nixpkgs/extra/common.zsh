e124844bf97e951ccbd3bdb749277ddb0adaec46
Lucas Denefle <ldenefle@gmail.com>
2020-11-24 17:26:30 +0000

    adds some shortcuts

--- nixpkgs/.config/nixpkgs/extra/common.zsh
+++ nixpkgs/.config/nixpkgs/extra/common.zsh
@@ -58,3 +58,16 @@ setopt AUTO_CD
 
 # Direnv 
 eval "$(direnv hook zsh)"
+
+# Add some colors for fzf
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+ --color=fg:#b3b1ad,bg:#0f1419,hl:#59c2ff
+ --color=fg+:#dadde0,bg+:#34455a,hl+:#ffcc66
+ --color=info:#f9af4f,prompt:#91b362,pointer:#cbccc6
+ --color=marker:#f9af4f,spinner:#f9af4f,header:#d4bfff'
+
+# Converge shortcuts
+alias gd='cd ~/_CODE/Converge/geodude-hw-app'
+alias on='cd ~/_CODE/Converge/onix-hw-app'
+alias ts='cd ~/_CODE/Converge/tilt-sensor-hw-app'
+alias vim='nvim'
