c0000e87390e474c529692a1404982b60f37c5f9
Lucas Denefle <ldenefle@gmail.com>
2021-12-24 10:56:55 +0100

    fix: support for ble in dwm-bar

---
+++ bin/.local/bin/bluetooth_headphones_status.sh
@@ -0,0 +1,38 @@
+#!/bin/zsh
+DIR=$(dirname $0)
+CMD=$1
+
+# Get the first bluetooth device info
+# This comes as a CSV
+BT_SINK=$(pacmd list-cards | $DIR/bt_dev_info.awk | grep "bluez" | head -n 1)
+
+if [ ! -z ${BT_SINK} ]; then
+  # if there is a device connected, then we can either list or change profile
+  # otherwise we do nothing
+  id=$(echo ${BT_SINK} | cut -d "," -f 1)
+  current_profile=$(echo ${BT_SINK} | cut -d "," -f 4)
+  case ${current_profile} in
+    "<a2dp_sink>")
+      icon="A2DP"
+      next_profile="handsfree_head_unit"
+      ;;
+    "<handsfree_head_unit>")
+      icon="HFP"
+      next_profile="a2dp_sink"
+      ;;
+    *)
+      icon="unknown"
+      next_profile=""
+      ;;
+  esac
+  case $CMD in
+    "toggle_profile")
+      pacmd set-card-profile ${id} ${next_profile}
+      ;;
+    *)
+      echo "${icon}"
+      ;;
+  esac
+else
+  echo ""
+fi
