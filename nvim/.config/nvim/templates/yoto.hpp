6a36d0528f3f2281a4acc7627c8b2b5eacc44d41
Lucas Denefle <ldenefle@gmail.com>
2025-07-21 16:07:14 +0100

    add templates for yoto

---
+++ nvim/.config/nvim/templates/yoto.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file {{_file_name_}}
+ * @author {{_author_}} - {{_email_}}
+ * @date {{_date_}}
+ * @brief {{_cursor_}}
+ *
+ * @copyright   © Yoto LTD, 2025. All rights reserved
+ */
+
+#ifndef {{_upper_file_}}_H_
+#define {{_upper_file_}}_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// INCLUDES *******************************************************************/
+
+// DEFINES ********************************************************************/
+
+// TYPEDEFS *******************************************************************/
+
+// GLOBAL VARIABLES ***********************************************************/
+
+// FUNCTION PROTOTYPES ********************************************************/
+
+// INLINE FUNCTIONS ***********************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* {{_upper_file_}}_H_ */
