13160ae819d987f56076750f73711f0119325635
Lucas Denefle <ldenefle@gmail.com>
2026-03-17 10:29:50 +0000

    Add jj and jjui configuration

---
+++ jjui/.config/jjui/config.lua
@@ -0,0 +1,38 @@
+-- ~/.config/jjui/config.lua
+function setup(config)
+
+  config.action("diff.move_down", function()
+    jjui.builtin.diff.half_page_down()
+  end, {desc= "diff move down", key ="ctrl+f", scope = "diff"} )
+
+  config.action("show diff in diffnav", function()
+    local change_id = context.change_id()
+    if not change_id or change_id == "" then
+      flash({ text = "No revision selected", error = true })
+      return
+    end
+
+    exec_shell(string.format("jj diff -r %q --git --color always | diffnav", change_id))
+  end, { desc = "show diff in diffnav", key = "ctrl+d", scope = "revisions" })
+
+  config.action("edit file", function()
+    local function first_hunk_new_lineno(git_diff)
+      for line in git_diff:gmatch("[^\n]+") do
+        if line:sub(1, 3) == "@@ " then
+          local new_start = line:match("%+(%d+)")
+          if new_start then
+            return tonumber(new_start)
+          end
+        end
+      end
+      return nil
+    end
+
+    local diff = jj("diff", "--git", "-r", context.change_id(), context.file())
+    local line_number = first_hunk_new_lineno(diff)
+    exec_shell(string.format("nvim +%q %q", line_number, context.file()))
+  end, {
+    scope = "revisions.details",
+    key = "e",
+  })
+end
